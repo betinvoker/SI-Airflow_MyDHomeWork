498db95849304d01a107bb77441c38e84df31c73
betinvoker <betinvoker1797@gmail.com>
2022-10-02 22:22:47 +0300

    New task create table from database

--- materials/dags/tvmaze.py
+++ materials/dags/tvmaze.py
@@ -1,7 +1,7 @@
 import json
-import pandas as pd
 from datetime import datetime
 
+import pandas as pd
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
@@ -10,13 +10,15 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from pandas import json_normalize
 
+
 def _process_transmission_tv(ti):
     transmission_tv = ti.xcom_pull(task_ids="extract_transmission")
 
     df = pd.DataFrame(transmission_tv)
-    df = df[['id', 'name', 'season', 'number', 'airdate', 'airstamp', 'runtime', 'url']]
+    df = df[["id", "name", "season", "number", "airdate", "airstamp", "url"]]
 
     df.to_csv("/tmp/process_transmission_tv.csv", index=False, header=False, sep="\002")
+
 
 def _store_transmission_tv():
     hook = PostgresHook(postgres_conn_id="postgres")
@@ -25,22 +27,22 @@ def _store_transmission_tv():
         filename="/tmp/process_transmission_tv.csv",
     )
 
+
 with DAG(
     "tvmaze", start_date=datetime(2022, 1, 1), schedule_interval="@daily", catchup=False
 ) as dag:
 
-    create_table = PostgresOperator(
+    create_meta_table = PostgresOperator(
         postgres_conn_id="postgres",
-        task_id="create_table",
+        task_id="create_meta_table",
         sql="""
         CREATE TABLE IF NOT EXISTS transmission_tv(
             id INTEGER NOT NULL,
             name TEXT NOT NULL,
-            season INTEGER,
-            number INTEGER,
+            season TEXT,
+            number TEXT,
             airdate DATE,
             airstamp TIMESTAMP,
-            runtime TEXT,
             url TEXT NOT NULL
         );""",
     )
@@ -48,13 +50,13 @@ with DAG(
     is_api_available = HttpSensor(
         task_id="is_api_available",
         http_conn_id="tvmaz_api",
-        endpoint="schedule/",
+        endpoint="schedule/web?date=2022-09-01&country=RU",
     )
 
     extract_transmission = SimpleHttpOperator(
         task_id="extract_transmission",
         http_conn_id="tvmaz_api",
-        endpoint="schedule/",
+        endpoint="schedule/web?date=2022-09-01&country=RU",
         method="GET",
         response_filter=lambda response: json.loads(response.text),
         log_response=True,
@@ -68,10 +70,26 @@ with DAG(
         task_id="store_transmission_tv", python_callable=_store_transmission_tv
     )
 
+    create_table = PostgresOperator(
+        postgres_conn_id="database",
+        task_id="create_table",
+        sql="""
+        CREATE TABLE IF NOT EXISTS transmission_tv(
+            id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            season TEXT,
+            number TEXT,
+            airdate DATE,
+            airstamp TIMESTAMP,
+            url TEXT NOT NULL
+        );""",
+    )
+
     (
-        create_table
+        create_meta_table
         >> is_api_available
         >> extract_transmission
         >> process_transmission_tv
         >> store_transmission_tv
+        >> create_table
     )
