64cbc02095a480421bf173ef0d4a6e3d68e12673
betinvoker <betinvoker1797@gmail.com>
2022-10-04 00:28:07 +0300

    final dag

--- materials/dags/tvmaze.py
+++ materials/dags/tvmaze.py
@@ -21,7 +21,7 @@ def _process_transmission_tv(ti):
 
 
 def _store_transmission_tv():
-    hook = PostgresHook(postgres_conn_id="postgres")
+    hook = PostgresHook(postgres_conn_id="database")
     hook.copy_expert(
         sql="""COPY transmission_tv FROM stdin WITH DELIMITER '\002' """,
         filename="/tmp/process_transmission_tv.csv",
@@ -31,44 +31,6 @@ def _store_transmission_tv():
 with DAG(
     "tvmaze", start_date=datetime(2022, 1, 1), schedule_interval="@daily", catchup=False
 ) as dag:
-
-    create_meta_table = PostgresOperator(
-        postgres_conn_id="postgres",
-        task_id="create_meta_table",
-        sql="""
-        CREATE TABLE IF NOT EXISTS transmission_tv(
-            id INTEGER NOT NULL,
-            name TEXT NOT NULL,
-            season TEXT,
-            number TEXT,
-            airdate DATE,
-            airstamp TIMESTAMP,
-            url TEXT NOT NULL
-        );""",
-    )
-
-    is_api_available = HttpSensor(
-        task_id="is_api_available",
-        http_conn_id="tvmaz_api",
-        endpoint="schedule/web?date=2022-09-01&country=RU",
-    )
-
-    extract_transmission = SimpleHttpOperator(
-        task_id="extract_transmission",
-        http_conn_id="tvmaz_api",
-        endpoint="schedule/web?date=2022-09-01&country=RU",
-        method="GET",
-        response_filter=lambda response: json.loads(response.text),
-        log_response=True,
-    )
-
-    process_transmission_tv = PythonOperator(
-        task_id="process_transmission_tv", python_callable=_process_transmission_tv
-    )
-
-    store_transmission_tv = PythonOperator(
-        task_id="store_transmission_tv", python_callable=_store_transmission_tv
-    )
 
     create_table = PostgresOperator(
         postgres_conn_id="database",
@@ -85,11 +47,63 @@ with DAG(
         );""",
     )
 
+    is_api_available = HttpSensor(
+        task_id="is_api_available",
+        http_conn_id="tvmaz_api",
+        endpoint="schedule/web?date=2022-09-05&country=RU",
+    )
+
+    extract_transmission = SimpleHttpOperator(
+        task_id="extract_transmission",
+        http_conn_id="tvmaz_api",
+        endpoint="schedule/web?date=2022-09-05&country=RU",
+        method="GET",
+        response_filter=lambda response: json.loads(response.text),
+        log_response=True,
+    )
+
+    process_transmission_tv = PythonOperator(
+        task_id="process_transmission_tv", python_callable=_process_transmission_tv
+    )
+
+    store_transmission_tv = PythonOperator(
+        task_id="store_transmission_tv", python_callable=_store_transmission_tv
+    )
+
+    create_table_analysis = PostgresOperator(
+        postgres_conn_id="database",
+        task_id="create_table_analysis",
+        sql="""
+        CREATE TABLE IF NOT EXISTS count_transmissions(
+            airdate DATE NOT NULL,
+            count INTEGER
+        );""",
+    )
+
+    store_count_series = PostgresOperator(
+        postgres_conn_id="database",
+        task_id="store_count_series",
+        sql="""
+        INSERT INTO count_transmissions
+            SELECT airdate, COUNT(id)
+            FROM transmission_tv
+            GROUP BY airdate
+        ;""",
+    )
+
+    truncate_table_analysis = PostgresOperator(
+        postgres_conn_id="database",
+        task_id="truncate_table_analysis",
+        sql="""TRUNCATE TABLE transmission_tv;""",
+    )
+
     (
-        create_meta_table
+        create_table
         >> is_api_available
         >> extract_transmission
         >> process_transmission_tv
         >> store_transmission_tv
-        >> create_table
+        >> create_table_analysis
+        >> store_count_series
+        >> truncate_table_analysis
     )
--- materials/includes/images/stock_data.py
+++
@@ -1,6 +0,0 @@
-import pickle
-import yfinance as yf
-
-tsla = yf.Ticker('TSLA')
-
-print(tsla.recommendations["To Grade"].value_counts().keys()[0])
